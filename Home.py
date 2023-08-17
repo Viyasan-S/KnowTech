6c87a727ee590c71614b7a9734a109078d0dabee
Viyasan-S <131290259+Viyasan-S@users.noreply.github.com>
2023-08-17 20:50:10 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -840,6 +840,14 @@ def community_page():
         datascience("AR/VR")
     if community == "Quantum Computing":
         datascience("Quantum Computing")
+
+async def ask_bot_async(prompt):
+    bot = await create_chatbot()
+    response = await bot.ask(prompt=prompt, conversation_style=ConversationStyle.creative, simplify_response=True)
+    bot_response = response['text']
+    bot_response = re.sub(r"\[\^\d+\^\]", " ", bot_response)
+    await bot.close()
+    return bot_response
     
 def chatbot_page():
     
@@ -880,7 +888,10 @@ def chatbot_page():
 
         with st.chat_message("assistant"):
             message_placeholder = st.empty()
-            full_response = asyncio.run(ask_bot(prompt))
+
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            full_response = loop.run_until_complete(ask_bot_async(prompt))
             
         st.session_state.messages.append(
             {"role": "assistant", "content": full_response}
