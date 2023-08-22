5db0b8e831b1e75a5d758a5d0dfd57ab63b9411d
Viyasan-S <131290259+Viyasan-S@users.noreply.github.com>
2023-08-22 21:19:53 +0530

    Update self_and_mentor.py

--- self_and_mentor.py
+++ self_and_mentor.py
@@ -207,7 +207,7 @@ def Mentorship(Mentors):
     if st.button("Request"):
         st.success("Request Sent Successfully! He/She will contact you soon")
     st.write('\n')
-    st.subheader("Experience & Qulifications")
+    st.subheader("Experience & Qualifications")
     st.write(
         """
     - ✔️ 7 Years expereince extracting actionable insights from data
